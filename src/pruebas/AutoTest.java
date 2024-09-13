5b2b8bf8cb6feea4b583fae94e66485027724f74
Alejandro Hugo Herms <hermsale@gmail.com>
2024-09-13 11:12:48 -0300

    update

--- src/pruebas/AutoTest.java
+++ src/pruebas/AutoTest.java
@@ -17,11 +17,11 @@ public class AutoTest{
     // tenemos que pasar el valor del metodo crearAuto a un String resultado, debido a que no se puede comparar directamente con el metodo. Si intentamos hacer crearAuto()=="Patente: AA076UL marca: Ford modelo: Fiesta color: Gris" . se esta queriendo comparar una cadena de manera incorrecta. La manera correcta es usando el metodo equals. Pero 
     public void TestCrearAuto(){
         // guardamos el array de texto en una variable String
-        String resultado = CrearAuto();
+        // String resultado = CrearAuto();
         // comparamos el resultado con el esperado
-        System.out.println(resultado);
-        //  utilizamos el metodo equals para comparar el valor del texto, con el esperado
-        if(resultado.equals("Patente: AA076UL marca: Ford modelo: Fiesta color: Gris")){
+        // System.out.println(resultado);
+        //  utilizamos el metodo equals para comparar el valor del texto, con el esperado . se puede utilizar en el mismo metodo o en el resultado.equals 
+        if(CrearAuto().equals("Patente: AA076UL marca: Ford modelo: Fiesta color: Gris")){
             System.out.println("Auto creado con exito");
         }else{
             System.out.println("Hubo un error");
